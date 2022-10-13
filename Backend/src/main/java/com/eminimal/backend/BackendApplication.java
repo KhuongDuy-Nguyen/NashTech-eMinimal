530b162b8118a2de2e9a3154ee0e68ddc0b830b0
Khương Duy <duyntkh@gmail.com>
2022-10-13 23:32:36 +0700

    Update BackendApplication.java
    Remove unnecessary library

--- Backend/src/main/java/com/eminimal/backend/BackendApplication.java
+++ Backend/src/main/java/com/eminimal/backend/BackendApplication.java
@@ -1,14 +1,7 @@
 package com.eminimal.backend;
 
-import com.eminimal.backend.models.*;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
-import org.hibernate.tool.schema.TargetType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 import java.util.EnumSet;
@@ -19,5 +12,6 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
 
 }
