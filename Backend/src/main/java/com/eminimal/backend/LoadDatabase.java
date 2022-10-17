48c83af4d6abf51c5db1edb7ea6b6fa8d53c2157
Khương Duy <duyntkh@gmail.com>
2022-10-17 17:26:53 +0700

    Add insert account

--- Backend/src/main/java/com/eminimal/backend/LoadDatabase.java
+++ Backend/src/main/java/com/eminimal/backend/LoadDatabase.java
@@ -2,8 +2,10 @@ package com.eminimal.backend;
 
 import com.eminimal.backend.models.Category;
 import com.eminimal.backend.models.Product;
+import com.eminimal.backend.models.User;
 import com.eminimal.backend.repository.CategoryRepository;
 import com.eminimal.backend.repository.ProductRepository;
+import com.eminimal.backend.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -18,10 +20,11 @@ public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(CategoryRepository categoryRepository, ProductRepository productRepository){
+    CommandLineRunner initDatabase(CategoryRepository categoryRepository, ProductRepository productRepository, UserRepository userRepository){
         return args -> {
             productRepository.deleteAll();
             categoryRepository.deleteAll();
+            userRepository.deleteAll();
 
 //          Insert Category
             log.info("Inserting --> " + categoryRepository.save(new Category("Sofas", "Tong hop cac loai sofa")));
@@ -74,7 +77,6 @@ public class LoadDatabase {
                     ))
             );
 
-
             log.info("Inserting --> " + productRepository.save(
                 new Product("Den trang tri hinh con gau 1",
                         "Day la den trang tri cuc cute",
@@ -112,6 +114,14 @@ public class LoadDatabase {
                             30f,
                             99,
                             categoryRepository.findCategoriesByCategoryName("Lighting")
+                    ))
+            );
+
+            log.info("Inserting --> " + userRepository.save(
+                    new User(
+                            "admin",
+                            "123",
+                            "admin@gmail.com"
                     ))
             );
 
