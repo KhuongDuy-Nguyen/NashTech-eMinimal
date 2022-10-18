56adf06703ab467a1beb343e1aed0e851fd14327
Khương Duy <duyntkh@gmail.com>
2022-10-18 19:15:23 +0700

    remove library

--- Backend/src/main/java/com/eminimal/backend/services/impl/ProductServiceImpl.java
+++ Backend/src/main/java/com/eminimal/backend/services/impl/ProductServiceImpl.java
@@ -1,29 +1,20 @@
 package com.eminimal.backend.services.impl;
 
-import com.eminimal.backend.LoadDatabase;
 import com.eminimal.backend.models.Category;
 import com.eminimal.backend.models.Product;
-import com.eminimal.backend.repository.CategoryRepository;
 import com.eminimal.backend.repository.ProductRepository;
-import org.modelmapper.ModelMapper;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
-import java.util.logging.Logger;
 
 @Service
 public class ProductServiceImpl implements com.eminimal.backend.services.ProductService {
 
     @Autowired
     private ProductRepository repository;
-
-    @Autowired
-    private ModelMapper modelMapper;
-
-    private static final org.slf4j.Logger log = LoggerFactory.getLogger(ProductServiceImpl.class);
-
 
 //  Find product
     @Override
@@ -57,15 +48,25 @@ public class ProductServiceImpl implements com.eminimal.backend.services.Product
 //  Update product
     @Override
     public Product updateProduct(UUID id, Product newProduct){
-        return repository.findById(id).map(product -> {
-            product = modelMapper.map(newProduct, Product.class);
-            product.setProductID(id);
-            product.setDateUpdate(new Date());
-            return repository.save(product);
-        }).orElseGet(() ->{
-            newProduct.setProductID(id);
-            return repository.save(newProduct);
-        });
+        Product product =  repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Can't found product with id:" + id));
+
+        product.setProductID(id);
+        product.setProductName(newProduct.getProductName());
+        product.setProductDesc(newProduct.getProductDesc());
+        product.setProductImage(newProduct.getProductImage());
+        product.setProductCost(newProduct.getProductCost());
+
+        product.setProductSale(newProduct.getProductSale());
+        product.setProductRating(newProduct.getProductRating());
+        product.setProductAmount(newProduct.getProductAmount());
+
+        product.setDateCreate(newProduct.getDateCreate());
+        product.setDateUpdate(new Date());
+        product.setDateSale(newProduct.getDateSale());
+
+        product.setCategories(newProduct.getCategories());
+
+        return repository.save(product);
     }
 
 }
