e4bc64e0e193a99c8e54a30b8ef7dd5ca464013f
Khương Duy <duyntkh@gmail.com>
2022-10-24 10:08:50 +0700

    Create CustomUserDetails.java

---
+++ Backend/src/main/java/com/eminimal/backend/models/users/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package com.eminimal.backend.models.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    Users users;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return users.getUserPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return users.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
