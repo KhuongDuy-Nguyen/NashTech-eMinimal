90d794832d58dba4f5350984138b28924b264126
KhuongDuy-Nguyen <duyntkh@gmail.com>
2022-11-09 06:59:23 +0700

    Update ErrorAuth.js

--- Fontend/src/utils/ErrorAuth.js
+++ Fontend/src/utils/ErrorAuth.js
@@ -1,16 +1,18 @@
-import { message } from "antd";
+import ShowMessage from "./message";
 
-export default function ErrorAuth(err){
-    if(err.response.status === 401){
-        message.error(
-          "You are not authorized to access this page. Try login again."
-        );
-        localStorage.removeItem("token");
-        setTimeout(() => {
-            window.location.href = "/login";
-        }, 1000);
-    }else{
-        console.log(err);
-        message.error(err.response.data.message);
-    }
+function ErrorAuth(err) {
+  console.log(err);
+  if (err.response.status === 401) {
+    ShowMessage("error","You are not authorized to access this page. Try login again.");
+    localStorage.removeItem("token");
+    setTimeout(() => {
+      window.location.href = "/auth";
+    }, 1000);
+  } else {
+    console.log("Run ErrorAuth");
+    console.log(err);
+    ShowMessage("error",err.response.data.message);
+  }
 }
+
+export default ErrorAuth;
